16ec64bcd8550566cbb08457683e6e8360ab8409
lovisgod <olifedayo94@gmail.com>
2019-04-10 00:24:04 +0100

    FT-created getUser endpoint

--- src/components/user/UserController.js
+++ src/components/user/UserController.js
@@ -4,12 +4,12 @@ import User from '../user/userModel';
 class UserController {
 
     constructor() {
-        this.getUser = this.getUser.bind(this);
+        this.getUserProfile = this.getUserProfile.bind(this);
         this.getUserAnswer = this.getUserAnswer.bind(this);
         this.updateUser = this.updateUser.bind(this);
     }
 
-     getUser = (req, res) => {
+     getUserProfile = (req, res) => {
         const {useremail} = req.params;
         let userDetails = User.findOne(useremail);
         if(!userDetails){

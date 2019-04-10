3b746e47d9c09ff098a90df1397684a3fedce234
Olatunbosun Adeoye <adex001@gmail.com>
2019-04-11 00:18:56 +0100

    removed redundant unused file

--- src/routes/user.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const {
-  catchErrors,
-} = require('../helpers');
-
-router.post('/login', catchErros(auth.login));
-router.post('/register', catchErrors(auth.register));
-
-exports = router;

aeb34c974b0f651e9058d72f423c7a65cb2d8123
Timothy Onyiuke <timothyonyiuke@gmail.com>
2019-04-10 18:39:05 +0100

    Remove duplicate route

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,12 +2,8 @@ const router = require('express').Router();
 const {
   catchErrors,
 } = require('../helpers');
+import UserController from '../components/user/UserController';
 
-import UserController from '../components/user/UserController'
-import auth from '../components/auth/AuthController';
-
-router.post('/login', catchErrors(auth.login));
-router.post('/register', catchErrors(auth.register));
 router.get('/userDetails', UserController.getUserProfile);
 router.put('/update', UserController.updateUserProfile);
 

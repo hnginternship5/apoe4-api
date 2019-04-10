35a606e1851f3e70c7f6a7c9a2b3fda1f1e2dcf0
Albert Oboh <30736063+a-oboh@users.noreply.github.com>
2019-04-10 11:42:51 +0100

    created first question schema

---
+++ src/components/questions/registerQuestionModel.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+import timestamps from 'mongoose-timestamp';
+
+const registerQuestionSchema = new Schema({
+    firstName: {
+        type: Number,
+        required: true,
+    },
+    familyHistory: {
+        type: Boolean,
+        default: false,
+        required: true,
+    },
+    dailyActivities: {
+        type: String,
+        required: true,
+    },
+    familiar: {
+        type: Boolean,
+        default: true,
+        required: true,
+    },
+    variant: {
+        type: String,
+        enum: ['Epsilon2', 'Epsilon3', 'Epsilon4'],
+        required: true,
+    },
+    test: {
+        type: Boolean,
+        required: true,
+    },
+    highBloodPressure: {
+        type: Boolean,
+        required: true,
+    },
+    lastCheckup: {
+        type: String,
+        required: true,
+    },
+});

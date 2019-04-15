bd7402826d3b24517ca0261dabc28621da3d54d4
YourFavouriteOreo <adiwani98@gmail.com>
2019-04-15 22:45:51 +0400

    Check Auth into Request and Question Owning

--- src/routes/question.js
+++ src/routes/question.js
@@ -1,21 +1,23 @@
 import { Router } from 'express';
+import checkAuth from '../middleware/check-auth';
+import helpers from '../helpers';
 import QuestionController from '../components/questions/questionController';
 
 const questionRouter = Router();
 //post questions after register
-questionRouter.post('/register-question', QuestionController.registerQuestions);
+questionRouter.post('/register-question',checkAuth,helpers.reassignOwner, QuestionController.registerQuestions);
 
 // //post daily questions
 // questionRouter.post('/daily-question', QuestionController.dailyQuestions);
 
 //post morning questions
-questionRouter.post('/morning-question', QuestionController.morningQuestions);
+questionRouter.post('/morning-question',checkAuth,helpers.reassignOwner, QuestionController.morningQuestions);
 
 //post noon questions
-questionRouter.post('/noon-question', QuestionController.noonQuestions);
+questionRouter.post('/noon-question',checkAuth,helpers.reassignOwner, QuestionController.noonQuestions);
 
 //post night questions
-questionRouter.post('/night-question', QuestionController.nightQuestions);
+questionRouter.post('/night-question',checkAuth,helpers.reassignOwner, QuestionController.nightQuestions);
 
 
 module.exports = questionRouter;
